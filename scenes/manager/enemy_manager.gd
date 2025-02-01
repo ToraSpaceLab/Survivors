713ce7e224435eca53520864d8ad1789e94135c4
Tora <tora0a0@gmail.com>
2025-02-01 19:00:29 -0400

    Preventing invalid spawning

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -14,17 +14,35 @@ func _ready() -> void:
 	base_spawn_time = timer.wait_time
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
-	
-	
+
+
+func get_spawn_position():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return Vector2.ZERO
+
+	var spawn_position = Vector2.ZERO
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	for i in 8:
+		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+
+		var query_paramaters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1 << 0)
+		var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_paramaters)
+
+		if result.is_empty():
+			break
+		else:
+			random_direction = random_direction.rotated(deg_to_rad(90))
+
+	return spawn_position
+
+
 func on_timer_timeout():
 	timer.start()
 	
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
-
-	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-	var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
 
 	var enemy = basic_enemy_scene.instantiate() as Node2D
 	
@@ -33,7 +51,7 @@ func on_timer_timeout():
 		return
 		
 	entities_layer.add_child(enemy)
-	enemy.global_position = spawn_position
+	enemy.global_position = get_spawn_position()
 
 
 func on_arena_difficulty_increased(arena_difficulty: int):
