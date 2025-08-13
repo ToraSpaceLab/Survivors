7c54f3f3580ff31350776cb2d7a0e8d5a107129e
Tora <tora0a0@gmail.com>
2025-08-13 02:43:54 -0300

    Add wizard enemy

--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 
-@onready var visuals = $Visuals
-@onready var velocity_component = $VelocityComponent
+@onready var velocity_component: VelocityComponent = $VelocityComponent
+@onready var visuals: Node2D = $Visuals
 
 
 func _process(delta: float) -> void:
 	velocity_component.accelerate_to_player()
 	velocity_component.move(self)
-	
+
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
-		visuals.scale = Vector2(-move_sign, 1)
+		visuals.scale = Vector2(move_sign, 1)
